f421c51d18290a81cc6f2c7445cec5c66b37ccc6
Alberto Vena <kennyadsl@gmail.com>
2020-02-05 21:58:25 +0100

    Readd some elements required by specs

--- spec/features/caching/taxons_spec.rb
+++ spec/features/caching/taxons_spec.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require 'byebug'
 
 describe 'taxons', type: :feature, caching: true do
   let!(:taxonomy) { create(:taxonomy) }
   let!(:taxon) { create(:taxon, taxonomy: taxonomy, parent: taxonomy.root) }
 
   before do
-    # warm up the cache
+    # Warm up the cache
     visit spree.root_path
 
     clear_cache_events
@@ -18,12 +17,19 @@ describe 'taxons', type: :feature, caching: true do
     taxon.touch(:updated_at)
 
     visit spree.root_path
-    expect(cache_writes.count).to eq(2)
+
+    # Cache rewrites:
+    # - 2 x categories component
+    # - 1 x taxons list in search form
+    expect(cache_writes.count).to eq(3)
   end
 
   it "busts the cache when max_level_in_taxons_menu conf changes" do
     stub_spree_preferences(max_level_in_taxons_menu: 5)
     visit spree.root_path
+
+    # Cache rewrites:
+    # - 2 x categories component
     expect(cache_writes.count).to eq(2)
   end
 end
