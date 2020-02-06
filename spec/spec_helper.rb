1c6a39881ad92113602018a474a2029e6f3e5f53
Alberto Vena <kennyadsl@gmail.com>
2020-02-06 10:54:04 +0100

    Fix preference loading
    freeze_preferences is not present in all the Solidus versions.
    Also, there's no need to include those helpers since they are
    already included by solidus_dev_support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,6 @@ require 'rspec-activemodel-mocks'
 require 'solidus_dev_support/rspec/feature_helper'
 require 'spree/testing_support/caching'
 require 'spree/testing_support/order_walkthrough'
-require 'spree/testing_support/preferences'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -25,9 +24,10 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = false
 
-  config.include Spree::TestingSupport::Preferences
   config.before(:suite) do
-    Spree::TestingSupport::Preferences.freeze_preferences(SolidusStarterFrontend::Config)
+    if Spree::TestingSupport::Preferences.respond_to?(:freeze_preferences)
+      Spree::TestingSupport::Preferences.freeze_preferences(SolidusStarterFrontend::Config)
+    end
   end
 
   config.before(:each) do
