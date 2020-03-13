097818e9a630abfaf026297596c373f87e242b19
Mattia Roccoberton <mat@blocknot.es>
2020-03-13 16:29:50 +0100

    Update LocaleController spec

--- spec/requests/spree/locale_controller_spec.rb
+++ spec/requests/spree/locale_controller_spec.rb
@@ -2,56 +2,56 @@
 
 require 'spec_helper'
 
-RSpec.describe Spree::LocaleController, type: :controller do
-  include_context "fr locale"
+describe Spree::LocaleController, type: :request do
+  include_context 'fr locale'
 
   context 'switch_to_locale specified' do
-    context "available locale" do
+    context 'available locale' do
       it 'sets locale and redirects' do
-        get :set, params: { switch_to_locale: 'fr' }
+        get spree.locale_set_path(switch_to_locale: 'fr')
         expect(I18n.locale).to eq :fr
         expect(response).to redirect_to('/')
         expect(session[:locale]).to eq('fr')
-        expect(flash[:notice]).to eq(I18n.t("spree.locale_changed"))
+        expect(flash[:notice]).to eq(I18n.t('spree.locale_changed'))
       end
     end
 
-    context "unavailable locale" do
+    context 'unavailable locale' do
       it 'does not change locale and redirects' do
-        get :set, params: { switch_to_locale: 'klingon' }
+        get spree.locale_set_path(switch_to_locale: 'klingon')
         expect(I18n.locale).to eq :en
         expect(response).to redirect_to('/')
-        expect(flash[:error]).to eq(I18n.t("spree.locale_not_changed"))
+        expect(flash[:error]).to eq(I18n.t('spree.locale_not_changed'))
       end
     end
   end
 
   context 'locale specified' do
-    context "available locale" do
+    context 'available locale' do
       it 'sets locale and redirects' do
-        get :set, params: { locale: 'fr' }
+        get spree.locale_set_path(locale: 'fr')
         expect(I18n.locale).to eq :fr
         expect(response).to redirect_to('/')
-        expect(flash[:notice]).to eq(I18n.t("spree.locale_changed"))
+        expect(flash[:notice]).to eq(I18n.t('spree.locale_changed'))
       end
     end
 
-    context "unavailable locale" do
+    context 'unavailable locale' do
       it 'does not change locale and redirects' do
-        get :set, params: { locale: 'klingon' }
+        get spree.locale_set_path(locale: 'klingon')
         expect(I18n.locale).to eq :en
         expect(response).to redirect_to('/')
-        expect(flash[:error]).to eq(I18n.t("spree.locale_not_changed"))
+        expect(flash[:error]).to eq(I18n.t('spree.locale_not_changed'))
       end
     end
   end
 
   context 'both locale and switch_to_locale specified' do
     it 'uses switch_to_locale value' do
-      get :set, params: { locale: 'en', switch_to_locale: 'fr' }
+      get spree.locale_set_path(locale: 'en', switch_to_locale: 'fr')
       expect(I18n.locale).to eq :fr
       expect(response).to redirect_to('/')
-      expect(flash[:notice]).to eq(I18n.t("spree.locale_changed"))
+      expect(flash[:notice]).to eq(I18n.t('spree.locale_changed'))
     end
   end
 end
