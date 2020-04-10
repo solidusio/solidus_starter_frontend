527ca4d84bd6381a7284f3678ed6d96f0674c587
Mattia Roccoberton <mat@blocknot.es>
2020-04-10 09:30:47 +0200

    Set the system specs configuration option only for Solidus < 2.11
    From version 2.11 the systems specs option will be included.
    PR: `solidusio/solidus#3552`

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ require 'rspec/active_model/mocks'
 require 'solidus_dev_support/rspec/feature_helper'
 require 'spree/testing_support/caching'
 require 'spree/testing_support/order_walkthrough'
+require 'spree/testing_support/translations' unless Spree.solidus_gem_version < Gem::Version.new('2.11')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -27,7 +28,17 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = false
 
-  config.include(Devise::Test::ControllerHelpers, type: :controller) if defined? Devise
+  if Spree.solidus_gem_version < Gem::Version.new('2.11')
+    config.extend Spree::TestingSupport::AuthorizationHelpers::Request, type: :system
+
+    config.after(:each, type: :system) do |example|
+      missing_translations = page.body.scan(/translation missing: #{I18n.locale}\.(.*?)[\s<\"&]/)
+      if missing_translations.any?
+        puts "Found missing translations: #{missing_translations.inspect}\n"
+        puts "In spec: #{example.location}"
+      end
+    end
+  end
 
   if SolidusSupport.reset_spree_preferences_deprecated?
     config.before :suite do
@@ -49,13 +60,5 @@ RSpec.configure do |config|
 
   config.before(:each, type: :system, js: true) do
     driven_by :apparition
-  end
-
-  config.after(:each, type: :system) do |example|
-    missing_translations = page.body.scan(/translation missing: #{I18n.locale}\.(.*?)[\s<\"&]/)
-    if missing_translations.any?
-      puts "Found missing translations: #{missing_translations.inspect}"
-      puts "In spec: #{example.location}"
-    end
   end
 end
