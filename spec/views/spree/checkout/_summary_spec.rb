3a559691af3001e5ec90dbc8d2331cd46d1f424e
dinohamzic <dino@subtlebits.com>
2020-04-17 16:29:16 +0200

    Fix checkout summary spec

--- spec/views/spree/checkout/_summary_spec.rb
+++ spec/views/spree/checkout/_summary_spec.rb
@@ -2,10 +2,10 @@
 
 require 'spec_helper'
 
-describe "spree/checkout/_summary.html.erb", type: :view do
+describe 'spree/components/checkout/_checkout_summary.html.erb', type: :view do
   # Regression spec for https://github.com/spree/spree/issues/4223
-  it "does not use the @order instance variable" do
+  it 'does not use the @order instance variable' do
     order = stub_model(Spree::Order)
-    render partial: "spree/checkout/summary", locals: { order: order }
+    render partial: 'spree/components/checkout/checkout_summary', locals: { order: order }
   end
 end
