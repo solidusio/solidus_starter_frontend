6ef2ad3693d24bc2531b36bb76e9ddbab02049c1
Paolo Tatone <paolotatone@nebulab.it>
2020-07-31 18:30:34 +0200

    Restore locale selector as a component

--- app/assets/javascripts/spree/frontend/locale_selector.js
+++ app/assets/javascripts/spree/frontend/locale_selector.js
@@ -1,5 +1,7 @@
-$(function() {
-  $('#locale_selector select').change(function() {
-    this.form.submit();
+window.addEventListener('DOMContentLoaded', () => {
+  const localeSelector = document.querySelector('.locale-selector select');
+
+  localeSelector.addEventListener('change', (event) => {
+    event.target.form.submit();
   });
 });
