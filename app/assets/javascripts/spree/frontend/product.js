dbac54edc641be2d6dfe8085b8c2d0dee15032df
markmead <itsmarkmead@gmail.com>
2020-08-29 00:04:06 +0100

    Replace product price JS class with data attribute

--- app/assets/javascripts/spree/frontend/product.js
+++ app/assets/javascripts/spree/frontend/product.js
@@ -1,21 +1,22 @@
 window.addEventListener('DOMContentLoaded', () => {
   function updateVariantPrice(variant) {
-    const variantPrice = variant.dataset.price;
+    const variantPrice = variant.dataset.price
+    const priceEl = document.querySelector('[data-module="product-price"]')
+
     if (variantPrice) {
-      document.querySelector('.price.selling').innerHTML = variantPrice;
+      priceEl.innerHTML = variantPrice
     }
-  };
-
-  const radios = document.querySelectorAll('#product-variants input[type="radio"]');
-
-  if (radios.length > 0) {
-    const selectedRadio = document
-      .querySelector('#product-variants input[type="radio"][checked="checked"]');
-
-    updateVariantPrice(selectedRadio);
   }
 
-  radios.forEach(radio => {
-    radio.addEventListener('click', () => updateVariantPrice(radio));
-  });
-});
+  const radios = document.querySelectorAll('#product-variants input[type="radio"]')
+
+  if (radios.length > 0) {
+    const selectedRadio = document.querySelector('#product-variants input[type="radio"][checked="checked"]')
+
+    updateVariantPrice(selectedRadio)
+  }
+
+  radios.forEach((radio) => {
+    radio.addEventListener('click', () => updateVariantPrice(radio))
+  })
+})
