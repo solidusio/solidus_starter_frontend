7f497b20efa91b3cebc337bc4f7b94f444cbff55
Paolo Tatone <paolotatone@nebulab.it>
2020-09-04 17:26:08 +0200

    Restore add to cart image selector

--- app/assets/javascripts/spree/frontend/product.js
+++ app/assets/javascripts/spree/frontend/product.js
@@ -1,4 +1,31 @@
 window.addEventListener('DOMContentLoaded', () => {
+  const radios = document.querySelectorAll('#product-variants input[type="radio"]');
+  const thumbnailsLinks = document.querySelectorAll('.js-product-thumbnail a');
+  const productImage = document.querySelector('.js-product-main-image');
+  const variantsThumbnails = document.querySelectorAll('.js-variant-thumbnail');
+
+  if (radios.length > 0) {
+    const selectedRadio = document
+      .querySelector('#product-variants input[type="radio"][checked="checked"]');
+
+    updateVariantPrice(selectedRadio);
+    updateVariantImages(selectedRadio.value);
+  }
+
+  radios.forEach(radio => {
+    radio.addEventListener('click', () => {
+      updateVariantPrice(radio);
+      updateVariantImages(radio.value);
+    });
+  });
+
+  thumbnailsLinks.forEach(thumbnailLink => {
+    thumbnailLink.addEventListener('click', (event) => {
+      event.preventDefault();
+      updateProductImage(thumbnailLink.href);
+    });
+  });
+
   function updateVariantPrice(variant) {
     const variantPrice = variant.dataset.price;
     if (variantPrice) {
@@ -6,16 +33,25 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   };
 
-  const radios = document.querySelectorAll('#product-variants input[type="radio"]');
+  function updateVariantImages(variantId) {
+    selector = "[data-js-product-thumbnail-variant-id='" + variantId + "']";
+    variantsThumbnailsToDisplay = document.querySelectorAll(selector);
 
-  if (radios.length > 0) {
-    const selectedRadio = document
-      .querySelector('#product-variants input[type="radio"][checked="checked"]');
+    variantsThumbnails.forEach(thumbnail => {
+      thumbnail.style.display = 'none';
+    });
 
-    updateVariantPrice(selectedRadio);
+    variantsThumbnailsToDisplay.forEach(thumbnail => {
+      thumbnail.style.display = 'list-item';
+    });
+
+    if(variantsThumbnailsToDisplay.length) {
+      variantFirstImage = variantsThumbnailsToDisplay[0].querySelector('a').href
+      updateProductImage(variantFirstImage);
+    }
+  };
+
+  function updateProductImage(imageSrc) {
+    productImage.src = imageSrc;
   }
-
-  radios.forEach(radio => {
-    radio.addEventListener('click', () => updateVariantPrice(radio));
-  });
 });
