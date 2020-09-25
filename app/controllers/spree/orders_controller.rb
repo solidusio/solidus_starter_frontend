613006b10d75e69bb5b963ef610d0a223b60c651
Mattia Roccoberton <mattiaroccoberton@nebulab.it>
2020-09-25 18:42:22 +0200

    Restore the "error" variable to keep the same code with solidus FE

--- app/controllers/spree/orders_controller.rb
+++ app/controllers/spree/orders_controller.rb
@@ -61,8 +61,8 @@ module Spree
       else
         begin
           @line_item = @order.contents.add(variant, quantity)
-        rescue ActiveRecord::RecordInvalid => e
-          @order.errors.add(:base, e.record.errors.full_messages.join(", "))
+        rescue ActiveRecord::RecordInvalid => error
+          @order.errors.add(:base, error.record.errors.full_messages.join(", "))
         end
       end
 
