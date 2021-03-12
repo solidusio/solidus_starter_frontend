9ac39ee17fd15c5b227d47fe7c91c499bc97b1cd
SamuelMartini <samueljmartini@gmail.com>
2021-03-12 11:29:36 +0100

    Convert controller order tracking to request

--- spec/requests/spree/current_order_tracking_spec.rb
+++ spec/requests/spree/current_order_tracking_spec.rb
@@ -2,31 +2,43 @@
 
 require 'spec_helper'
 
-describe 'current order tracking', type: :controller do
+describe 'current order tracking', type: :request, with_signed_in_user: true do
   let!(:store) { create(:store) }
   let(:user) { create(:user) }
 
-  controller(Spree::StoreController) do
-    def index
+  class TestController < Spree::StoreController
+    def create_order
+      @order = current_order(create_order_if_necessary: true)
+      head :ok
+    end
+
+    def not_create_order
       head :ok
     end
   end
+  
+  before do
+    Rails.application.routes.draw do
+      get '/test', to: 'test#create_order'
+      get '/test2', to: 'test#not_create_order'
+    end
+  end
+  after do
+    Rails.application.reload_routes!
+  end
 
-  let(:order) { FactoryBot.create(:order) }
 
   it 'automatically tracks who the order was created by & IP address' do
-    allow(controller).to receive_messages(try_spree_current_user: user)
-    get :index
-    expect(controller.current_order(create_order_if_necessary: true).created_by).to eq controller.try_spree_current_user
-    expect(controller.current_order.last_ip_address).to eq "0.0.0.0"
+    get '/test'
+
+    expect(assigns[:order].created_by).to eq user
+    expect(assigns[:order].last_ip_address).to eq "127.0.0.1"
   end
 
   context "current order creation" do
-    before { allow(controller).to receive_messages(try_spree_current_user: user) }
-
     it "doesn't create a new order out of the blue" do
       expect {
-        get :index
+        get '/test2'
       }.not_to change { Spree::Order.count }
     end
   end
