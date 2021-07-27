544e8d0612d03613bb6d389343c062441883164a
George Mendoza <gsmendoza@gmail.com>
2021-07-27 17:02:57 +0800

    Copy solidus_auth_devise controllers
    Fixes
    
    ```
        An error occurred while loading spec_helper.
        Failure/Error: Spree::UserConfirmationsController,
    
        NameError:
          uninitialized constant Spree::UserConfirmationsController
        # ./lib/solidus_starter_frontend/engine.rb:20:in `block in <class:Engine>'
        # ./spec/dummy/config/environment.rb:5:in `<top (required)>'
        # ./spec/spec_helper.rb:15:in `require'
        # ./spec/spec_helper.rb:15:in `<top (required)>'
    ```

---
+++ app/controllers/spree/user_confirmations_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Spree::UserConfirmationsController < Devise::ConfirmationsController
+  helper 'spree/base', 'spree/store'
+
+  include Spree::Core::ControllerHelpers::Auth
+  include Spree::Core::ControllerHelpers::Common
+  include Spree::Core::ControllerHelpers::Order
+  include Spree::Core::ControllerHelpers::Store
+
+  protected
+
+  def after_confirmation_path_for(resource_name, resource)
+    signed_in?(resource_name) ? spree.signed_in_root_path(resource) : spree.login_path
+  end
+end
