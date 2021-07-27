c873a105a258a3f78a1f662159c58be209face82
George Mendoza <gsmendoza@gmail.com>
2021-07-27 18:01:33 +0800

    Copy unauthorized_redirect from solidus_auth_devise
    Fixes "Authorization Failure" error displayed on sign-in.
    
    Fixes
    
    ```
      1) Sign In should store the user previous location
         Failure/Error: fill_in "Email", with: @user.email
    
         Capybara::ElementNotFound:
           Unable to find field "Email" that is not disabled
         # ./spec/features/sign_in_spec.rb:31:in `block (2 levels) in <top (required)>'
    ```

--- lib/solidus_starter_frontend/engine.rb
+++ lib/solidus_starter_frontend/engine.rb
@@ -26,6 +26,26 @@ module SolidusStarterFrontend
           auth_controller.include SolidusStarterFrontend::Taxonomies
         end
       end
+
+      Spree::BaseController.unauthorized_redirect = -> do
+        if try_spree_current_user
+          flash[:error] = I18n.t('spree.authorization_failure')
+
+          if Spree::Auth::Engine.redirect_back_on_unauthorized?
+            redirect_back(fallback_location: spree.unauthorized_path)
+          else
+            redirect_to spree.unauthorized_path
+          end
+        else
+          store_location
+
+          if Spree::Auth::Engine.redirect_back_on_unauthorized?
+            redirect_back(fallback_location: spree.login_path)
+          else
+            redirect_to spree.login_path
+          end
+        end
+      end
     end
   end
 end
