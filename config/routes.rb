1b35df8754ed312576861d224f21fc94d47855e3
George Mendoza <gsmendoza@gmail.com>
2021-07-27 16:40:20 +0800

    Copy solidus_auth_devise frontend routes
    Fixes
    
    ```
      1) Accounts editing can edit an admin user
         Failure/Error: visit spree.login_path
    
         NoMethodError:
           undefined method `login_path' for #<Module:0x0000563aafaae480>
         # ./spec/features/account_spec.rb:7:in `block (3 levels) in <top (required)>'
    ```

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,40 @@
 Spree::Core::Engine.routes.draw do
   root to: 'home#index'
 
+  devise_for(:spree_user, {
+    class_name: 'Spree::User',
+    controllers: {
+      sessions: 'spree/user_sessions',
+      registrations: 'spree/user_registrations',
+      passwords: 'spree/user_passwords',
+      confirmations: 'spree/user_confirmations'
+    },
+    skip: [:unlocks, :omniauth_callbacks],
+    path_names: { sign_out: 'logout' },
+    path_prefix: :user,
+    router_name: :spree
+  })
+
+  resources :users, only: [:edit, :update]
+
+  devise_scope :spree_user do
+    get '/login', to: 'user_sessions#new', as: :login
+    post '/login', to: 'user_sessions#create', as: :create_new_session
+    match '/logout', to: 'user_sessions#destroy', as: :logout, via: Devise.sign_out_via
+    get '/signup', to: 'user_registrations#new', as: :signup
+    post '/signup', to: 'user_registrations#create', as: :registration
+    get '/password/recover', to: 'user_passwords#new', as: :recover_password
+    post '/password/recover', to: 'user_passwords#create', as: :reset_password
+    get '/password/change', to: 'user_passwords#edit', as: :edit_password
+    put '/password/change', to: 'user_passwords#update', as: :update_password
+    get '/confirm', to: 'user_confirmations#show', as: :confirmation if Spree::Auth::Config[:confirmable]
+  end
+
+  get '/checkout/registration', to: 'checkout#registration', as: :checkout_registration
+  put '/checkout/registration', to: 'checkout#update_registration', as: :update_checkout_registration
+
+  resource :account, controller: 'users'
+
   resources :products, only: [:index, :show]
 
   get '/locale/set', to: 'locale#set'
