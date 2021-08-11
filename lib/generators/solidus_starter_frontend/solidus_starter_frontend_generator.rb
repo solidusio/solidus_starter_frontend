f97404fcedc76117a90982bb8aaa090a86d61656
George Mendoza <gsmendoza@gmail.com>
2021-08-11 18:08:37 +0800

    Update SolidusStarterFrontendGenerator to copy the specs to the storefront app
    Goal
    ----
    
    As a solidus_starter_frontend contributor
    I want SolidusStarterFrontendGenerator to copy the test suite to the storefront app
    So that I can the run the test suite to verify that the generated frontend is working
    
    Considerations
    --------------
    
    We still want the specs to run on the gem. That is, we shouldn't be forced to
    generate the app in order to run the test suite there.
    
    Implications
    ------------
    
    This would mean that SolidusStarterFrontendGenerator would also generate a
    starter test suite for the storefronts it would generate.
    
    *Pros*
    
    * Users of SolidusStarterFrontendGenerator can lean on the test suite as they
      change their storefronts.
    
    *Cons*
    
    * We would be including the whole test suite, which may include
      * Low-quality tests
      * Tests that would make more sense in the context of the gem than in the
        context of a generated storefront.
    * We could be bloating the user's test suite.
    
    Thoughts
    --------
    
    Despite the cons, I think it would be difficult to curate a test suite
    specifically for the generated storefront.
    
    Furthermore, considering that we're already allowing the frontend code to be
    consumed directly by users, I think that it's okay to afford the test suite the
    same leniency we are allowing to the frontend code. As a plus, any improvements
    we make later on to the frontend code and its test suite will benefit both the
    gem and its consumers using its generated frontends.
    
    Scope
    -----
    
    In a succeeding ticket, we can update the CI to run the specs in the generated app.

--- lib/generators/solidus_starter_frontend/solidus_starter_frontend_generator.rb
+++ lib/generators/solidus_starter_frontend/solidus_starter_frontend_generator.rb
@@ -28,5 +28,18 @@ class SolidusStarterFrontendGenerator < Rails::Generators::Base
     inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/solidus_starter_frontend\n", before: %r{\*/}, verbose: true
     inject_into_file 'config/initializers/spree.rb', "require_relative Rails.root.join('lib/solidus_starter_frontend/config')\n", before: /Spree.config do/, verbose: true
     gsub_file 'app/assets/stylesheets/application.css', '*= require_tree', '* OFF require_tree'
+
+    # We can't use Rails' `generate` method here to call the generators. When
+    # the solidus_starter_frontend generator is used as a standalone program
+    # (instead of added to an app's Gemfile), `generate` won't be able to pick
+    # up the other generators that the gem provides.
+    #
+    # We're able to use Thor's `invoke` action here instead of `generate`.
+    # However, `invoke` only works once per generator you want to call. Please
+    # see https://stackoverflow.com/a/12029262/65925 for more details.
+    #
+    # See also https://github.com/nebulab/solidus_starter_frontend/pull/174#discussion_r685626737.
+    invoke 'solidus_starter_frontend:rspec'
+    invoke 'rspec:install'
   end
 end
