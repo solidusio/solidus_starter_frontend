cd968eced84f83dcd36b5e93874c1f199198856f
George Mendoza <gsmendoza@gmail.com>
2021-08-18 14:15:36 +0800

    Add option to skip specs when installing Solidus Starter Frontend
    Goal
    ----
    
    As a `solidus_starter_frontend` user
    
    I want the spec generation to be optional
    
    So that I can choose to exclude the specs when installing the starter
    frontend to my Solidus app
    
    Background
    ----------
    
    Brought up in https://github.com/nebulab/solidus_starter_frontend/pull/175#pullrequestreview-731371278:
    
    > As you said in the issue description, moving all the tests to the host
      application has several drawbacks, but it's consistent with the direction
      we're taken. I wonder if we could add an environment variable SKIP_SPECS so
      that users can choose not to copy them, though. WDYT?
    
    Possible reasons for choosing not to install the specs
    ------------------------------------------------------
    
    * I prefer using Test::Unit or I already have an existing Test::Unit
      test suite.
    * I'm concerned about the quality of the tests.
    * I'm concerned about the additional runtime of the tests.
    
    Implementation
    --------------
    
    See https://guides.rubyonrails.org/generators.html#adding-command-line-arguments

--- lib/generators/solidus_starter_frontend/solidus_starter_frontend_generator.rb
+++ lib/generators/solidus_starter_frontend/solidus_starter_frontend_generator.rb
@@ -3,6 +3,8 @@
 class SolidusStarterFrontendGenerator < Rails::Generators::Base
   source_root File.expand_path('../../..', __dir__)
 
+  class_option 'skip-specs', type: :boolean, default: false
+
   def install
     # Copy directories
     directory 'app', 'app'
@@ -29,17 +31,19 @@ class SolidusStarterFrontendGenerator < Rails::Generators::Base
     inject_into_file 'config/initializers/spree.rb', "require_relative Rails.root.join('lib/solidus_starter_frontend/config')\n", before: /Spree.config do/, verbose: true
     gsub_file 'app/assets/stylesheets/application.css', '*= require_tree', '* OFF require_tree'
 
-    # We can't use Rails' `generate` method here to call the generators. When
-    # the solidus_starter_frontend generator is used as a standalone program
-    # (instead of added to an app's Gemfile), `generate` won't be able to pick
-    # up the other generators that the gem provides.
-    #
-    # We're able to use Thor's `invoke` action here instead of `generate`.
-    # However, `invoke` only works once per generator you want to call. Please
-    # see https://stackoverflow.com/a/12029262/65925 for more details.
-    #
-    # See also https://github.com/nebulab/solidus_starter_frontend/pull/174#discussion_r685626737.
-    invoke 'solidus_starter_frontend:rspec'
-    invoke 'rspec:install'
+    unless options['skip-specs']
+      # We can't use Rails' `generate` method here to call the generators. When
+      # the solidus_starter_frontend generator is used as a standalone program
+      # (instead of added to an app's Gemfile), `generate` won't be able to pick
+      # up the other generators that the gem provides.
+      #
+      # We're able to use Thor's `invoke` action here instead of `generate`.
+      # However, `invoke` only works once per generator you want to call. Please
+      # see https://stackoverflow.com/a/12029262/65925 for more details.
+      #
+      # See also https://github.com/nebulab/solidus_starter_frontend/pull/174#discussion_r685626737.
+      invoke 'solidus_starter_frontend:rspec'
+      invoke 'rspec:install'
+    end
   end
 end
