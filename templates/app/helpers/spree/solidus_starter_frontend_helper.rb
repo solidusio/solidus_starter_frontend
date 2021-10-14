c279dd392705e467d13038ec8a5a1bc879c681fd
George Mendoza <gsmendoza@gmail.com>
2021-10-14 14:13:18 +0800

    Extract taxon_tree_with_base_class helper

--- templates/app/helpers/spree/solidus_starter_frontend_helper.rb
+++ templates/app/helpers/spree/solidus_starter_frontend_helper.rb
@@ -50,5 +50,19 @@ module Spree
     def generate_address_input_id(form, input_name)
       "#{form.object_name.gsub(/[\[\]]/, '[' => '_', ']' => '_')}#{input_name}"
     end
+
+    def taxon_tree_with_base_class(root_taxon, current_taxon, base_class, max_level = 1)
+      return '' if max_level < 1 || root_taxon.children.empty?
+      content_tag :ul, class: base_class do
+        taxons = root_taxon.children.map do |taxon|
+          css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'current' : nil
+          content_tag :li, class: css_class do
+           link_to(taxon.name, seo_url(taxon)) +
+             taxon_tree_with_base_class(taxon, current_taxon, nil, max_level - 1)
+          end
+        end
+        safe_join(taxons, "\n")
+      end
+    end
   end
 end
