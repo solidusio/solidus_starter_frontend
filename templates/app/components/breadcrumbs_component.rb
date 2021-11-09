64eadeacdaa0ba56b2083c3b52fe814cc8d03aae
George Mendoza <gsmendoza@gmail.com>
2021-11-09 15:08:56 +0800

    Inline breadcrumbs partial in component

--- templates/app/components/breadcrumbs_component.rb
+++ templates/app/components/breadcrumbs_component.rb
@@ -1,11 +1,54 @@
 # frozen_string_literal: true
 
 class BreadcrumbsComponent < ViewComponent::Base
+  attr_reader :taxon
+
   def initialize(taxon)
     @taxon = taxon
   end
 
   def call
-    render 'spree/components/navigation/breadcrumbs', taxon: @taxon
+    separator = '&nbsp;&raquo;&nbsp;'
+    base_class = 'breadcrumbs'
+
+    breadcrumbs = breadcrumbs(taxon, separator, "#{base_class}__content")
+
+    if breadcrumbs.present?
+      content_tag(:div, breadcrumbs, class: base_class)
+    end
+  end
+
+  private
+
+  def breadcrumbs(taxon, separator, breadcrumb_class = 'inline')
+    return '' if current_page?('/') || taxon.nil?
+
+    crumbs = [[t('spree.home'), helpers.spree.root_path]]
+
+    crumbs << [t('spree.products'), helpers.spree.products_path]
+    if taxon
+      crumbs += taxon.ancestors.collect { |ancestor| [ancestor.name, helpers.spree.nested_taxons_path(ancestor.permalink)] }
+      crumbs << [taxon.name, helpers.spree.nested_taxons_path(taxon.permalink)]
+    end
+
+    separator = raw(separator)
+
+    items = crumbs.each_with_index.collect do |crumb, index|
+      content_tag(:li, itemprop: 'itemListElement', itemscope: '', itemtype: 'https://schema.org/ListItem') do
+        link_to(crumb.last, itemprop: 'item') do
+          content_tag(:span, crumb.first, itemprop: 'name') + tag('meta', { itemprop: 'position', content: (index + 1).to_s }, false, false)
+        end + (crumb == crumbs.last ? '' : separator)
+      end
+    end
+
+    content_tag(
+      :nav,
+      content_tag(
+        :ol,
+        raw(items.map(&:mb_chars).join),
+        itemscope: '',
+        itemtype: 'https://schema.org/BreadcrumbList'),
+      class: breadcrumb_class
+    )
   end
 end
