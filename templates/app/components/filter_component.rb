5c635f89160884345dff0abb76a7a1ac37cae8e5
George Mendoza <gsmendoza@gmail.com>
2021-11-10 15:09:04 +0800

    Inline filter partial in view component

--- templates/app/components/filter_component.rb
+++ templates/app/components/filter_component.rb
@@ -8,6 +8,50 @@ class FilterComponent < ViewComponent::Base
   end
 
   def call
-    render 'spree/components/search/filter', local_assigns
+    # Props
+    filter = local_assigns.fetch(:filter, nil)
+    search_params = local_assigns.fetch(:search_params, nil)
+    title = filter[:name]
+    base_class = 'filter'
+
+    filter_list = filter_list(filter, "#{base_class}__list", search_params)
+
+    if filter_list.present?
+      contents = []
+      contents << content_tag(:h6, title, class: "#{base_class}__title") if title
+      contents << filter_list
+      safe_join(contents)
+    end
+  end
+
+  private
+
+  def filter_list(filter, css_class, search_params)
+    labels = filter[:labels] || filter[:conds].map {|m,c| [m,m]}
+    return if labels.empty?
+
+    content_tag :ul, class: css_class do
+      labels.each do |name, value|
+        label = "#{filter[:name]}_#{name}".gsub(/\s+/,'_')
+        checked = search_params &&
+          search_params[filter[:scope]] &&
+          search_params[filter[:scope]].include?(value.to_s) ? true : false
+
+        concat filter_list_item(filter, checked, label, value, name)
+      end
+    end
+  end
+
+  def filter_list_item(filter, checked, label, value, name)
+    content_tag :li do
+      concat check_box_tag(
+        "search[#{filter[:scope].to_s}][]",
+        value,
+        checked,
+        id: label)
+
+      # concat label_tag(label, name)
+      concat ("<label for='#{label}'>#{name}</label>").html_safe
+    end
   end
 end
