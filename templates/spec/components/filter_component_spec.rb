5ed337281050873aad0cb673d89b00058cc14299
George Mendoza <gsmendoza@gmail.com>
2021-11-10 15:38:01 +0800

    Pass search params explicitly to filter

--- templates/spec/components/filter_component_spec.rb
+++ templates/spec/components/filter_component_spec.rb
@@ -3,8 +3,14 @@ require "solidus_starter_frontend_helper"
 RSpec.describe FilterComponent, type: :component do
   describe '#call' do
     let(:filter) { Spree::Core::ProductFilters.price_filter }
+    let(:search_params) { {} }
 
-    let(:local_assigns) { { filter: filter } }
+    let(:local_assigns) do
+      {
+        filter: filter,
+        search_params: search_params
+      }
+    end
 
     let(:component) do
       render_inline(described_class.new(local_assigns))
@@ -19,6 +25,24 @@ RSpec.describe FilterComponent, type: :component do
     it 'renders a list of checkboxes for the filter labels' do
       expect(input_ids).to_not be_empty
       expect(input_ids.first).to eq('Price_Range_Under_$10.00')
+    end
+
+    context 'when a filter list item was checked' do
+      let(:search_params) do
+        { price_range_any: ["Under $10.00"] }
+      end
+
+      it 'renders as checked' do
+        expect(component.first('input')['checked']).to be_truthy
+      end
+    end
+
+    context 'when a filter list item was not checked' do
+      let(:search_params) { { } }
+
+      it 'renders as unchecked' do
+        expect(component.first('input')['checked']).to be_falsey
+      end
     end
   end
 end
