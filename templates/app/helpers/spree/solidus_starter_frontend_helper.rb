1ebbdcb373166bc47143731f9ceb3831a0b248f4
George Mendoza <gsmendoza@gmail.com>
2021-11-17 18:19:14 +0800

    Use FormBuilder methods for selected_shipping_rate_id
    HTML changed from
    
    ```
    <label class="radio-input" for="order_shipments_attributes__0_selected_shipping_rate_id_34">
              <input type="radio" name="order[shipments_attributes][0][selected_shipping_rate_id]" id="order_shipments_attributes__0_selected_shipping_rate_id_34" value="34" checked="checked" />
              UPS Ground (USD) $5.00 (+ $0.25 North America)
    </label>
    ```
    
    to
    
    ```
    <label class="radio-input" for="order_shipments_attributes_0_selected_shipping_rate_id_34">
            <input type="radio" value="34" checked="checked" name="order[shipments_attributes][0][selected_shipping_rate_id]" id="order_shipments_attributes_0_selected_shipping_rate_id_34" />
            UPS Ground (USD) $5.00 (+ $0.25 North America)
    </label>
    ```

--- templates/app/helpers/spree/solidus_starter_frontend_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Spree
-  module SolidusStarterFrontendHelper
-    def generate_selected_shipping_rate_id(form, input_name, rate)
-      "#{form.object_name.gsub(/[\[\]]/, '[' => '_', ']' => '_')}#{input_name}_#{rate.id}"
-    end
-  end
-end
