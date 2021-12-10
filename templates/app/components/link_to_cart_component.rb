f91f41fae98dcff9a8bd147e8544a30318d3f706
George Mendoza <gsmendoza@gmail.com>
2021-12-10 14:53:08 +0800

    Refactor LinkToCartComponent

--- templates/app/components/link_to_cart_component.rb
+++ templates/app/components/link_to_cart_component.rb
@@ -1,34 +1,23 @@
 # frozen_string_literal: true
 
 class LinkToCartComponent < ViewComponent::Base
-  attr_reader :text
-
   delegate :current_order, :spree, to: :helpers
 
-  def initialize(text = nil)
-    @text = text
-  end
-
   def call
-    link_to_cart(text)
+    link_to text.html_safe, spree.cart_path, class: "cart-info #{css_class}", title: 'Cart'
   end
 
   private
 
-  # Copied from https://github.com/solidusio/solidus-demo/blob/3c11395ef6bfda0b906f68dbd11ab5ff567da747/app/monkey_patches/spree/base_helper_monkey_patch.rb
-  # Added "Cart" title to link primarily to get the tests to pass.
-  def link_to_cart(text = nil)
-    text = text ? h(text) : t('spree.cart')
-    css_class = nil
+  def text
+    empty_current_order? ? '' : "<div class='link-text'>#{current_order.item_count}</div>"
+  end
 
-    if current_order.nil? || current_order.item_count.zero?
-      text = ""
-      css_class = 'empty'
-    else
-      text = "<div class='link-text'>#{current_order.item_count}</div>"\
-            #"<span class='amount'>#{current_order.display_total.to_html}</span>"
-      css_class = 'full'
-    end
-    link_to text.html_safe, spree.cart_path, class: "cart-info #{css_class}", title: 'Cart'
+  def css_class
+    empty_current_order? ? 'empty' : 'full'
+  end
+
+  def empty_current_order?
+    current_order.nil? || current_order.item_count.zero?
   end
 end
