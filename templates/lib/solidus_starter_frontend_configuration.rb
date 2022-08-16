2f00ecbc6b1ddea2fe5a3aaf338b640b7b31dd57
George Mendoza <gsmendoza@gmail.com>
2022-08-16 17:21:09 +0800

    Remove locale config

--- templates/lib/solidus_starter_frontend_configuration.rb
+++ templates/lib/solidus_starter_frontend_configuration.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class SolidusStarterFrontendConfiguration < Spree::Preferences::Configuration
-  preference :locale, :string, default: I18n.default_locale
-
   # Add your terms and conditions in app/views/spree/checkout/_terms_and_conditions.en.html.erb
   preference :require_terms_and_conditions_acceptance, :boolean, default: false
 end
