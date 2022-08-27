a0e1635fdb31b3474d351373fabd1aeb7c969319
George Mendoza <gsmendoza@gmail.com>
2022-08-27 16:41:19 +0800

    Set UserMailer to non-Spree version

--- template.rb
+++ template.rb
@@ -89,6 +89,9 @@ def copy_solidus_starter_frontend_files
   copy_file 'config/initializers/solidus_auth_devise_unauthorized_redirect.rb'
   copy_file 'config/initializers/canonical_rails.rb'
 
+  copy_file 'config/initializers/devise.rb', 'tmp/devise.rb'
+  append_file 'config/initializers/devise.rb', File.read('tmp/devise.rb')
+
   application <<~RUBY
     if Rails.autoloaders.main
       # Rails 7
