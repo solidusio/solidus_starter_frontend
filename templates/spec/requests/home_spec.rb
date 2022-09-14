8e55de03be51f5d7e79687b0d323fd9563baba0f
George Mendoza <gsmendoza@gmail.com>
2022-09-14 16:52:04 +0800

    No longer test changing the layout via the Solidus config
    The layout can now directly be set on the app's controllers.

--- templates/spec/requests/home_spec.rb
+++ templates/spec/requests/home_spec.rb
@@ -25,14 +25,5 @@ RSpec.describe 'Home layout', type: :request, with_signed_in_user: true do
       get spree.root_path
       expect(response).to render_template(layout: 'layouts/storefront')
     end
-
-    context "different layout specified in config" do
-      before { stub_spree_preferences(layout: 'layouts/application') }
-
-      it "renders specified layout" do
-        get spree.root_path
-        expect(response).to render_template(layout: 'layouts/application')
-      end
-    end
   end
 end
