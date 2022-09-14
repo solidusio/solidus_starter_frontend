f424a46090be2627e8d282bf1199ab790bc3145e
Elia Schito <elia@schito.me>
2022-09-14 18:22:46 +0200

    Extract the database_cleaner config to a support file

--- templates/spec/solidus_starter_frontend_helper.rb
+++ templates/spec/solidus_starter_frontend_helper.rb
@@ -48,19 +48,8 @@ RSpec.configure do |config|
 
   config.include Spree::TestingSupport::Preferences
 
-  config.before :suite do
-    DatabaseCleaner.clean_with :truncation
-  end
-
   config.before do
     ActiveJob::Base.queue_adapter = :test
-  end
-
-  # Around each spec check if it is a Javascript test and switch between using
-  # database transactions or not where necessary.
-  config.around(:each) do |example|
-    DatabaseCleaner.strategy = RSpec.current_example.metadata[:js] ? :truncation : :transaction
-    DatabaseCleaner.cleaning { example.run }
   end
 
   config.include ActiveJob::TestHelper
