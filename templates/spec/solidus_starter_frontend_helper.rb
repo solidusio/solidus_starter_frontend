2555e02c7c8012b9799aaa7a8adbfddc5c800342
Elia Schito <elia@schito.me>
2022-09-14 17:16:33 +0200

    Merge capybara support code from solidus_dev_support

--- templates/spec/solidus_starter_frontend_helper.rb
+++ templates/spec/solidus_starter_frontend_helper.rb
@@ -6,7 +6,6 @@ require 'rails-controller-testing'
 require 'rspec/active_model/mocks'
 
 require "view_component/test_helpers"
-require "capybara/rspec"
 
 # Requires factories and other useful helpers defined in spree_core.
 # START require 'solidus_dev_support/rspec/feature_helper'
@@ -94,30 +93,6 @@ RSpec.configure do |config|
     raise ZeitwerkNameError, message
   end
 end
-
-require 'capybara-screenshot/rspec'
-require 'webdrivers/chromedriver'
-
-# Allow to override the initial windows size
-CAPYBARA_WINDOW_SIZE = ENV.fetch('CAPYBARA_WINDOW_SIZE', '1920x1080').split('x', 2).map(&:to_i)
-CAPYBARA_WINDOW_WIDTH = CAPYBARA_WINDOW_SIZE[0]
-CAPYBARA_WINDOW_HEIGHT = CAPYBARA_WINDOW_SIZE[1]
-
-Capybara.javascript_driver = ENV.fetch('CAPYBARA_JAVASCRIPT_DRIVER', "solidus_chrome_headless").to_sym
-Capybara.default_max_wait_time = 10
-Capybara.server = :puma, { Silent: true } # A fix for rspec/rspec-rails#1897
-
-Capybara.drivers[:selenium_chrome_headless].tap do |original_driver|
-  Capybara.register_driver :solidus_chrome_headless do |app|
-    original_driver.call(app).tap do |driver|
-      driver.resize_window_to(
-        driver.current_window_handle, CAPYBARA_WINDOW_WIDTH, CAPYBARA_WINDOW_HEIGHT
-      )
-    end
-  end
-end
-
-require 'spree/testing_support/capybara_ext'
 
 dev_support_assets_preload = ->(*) {
   if Rails.application.respond_to?(:precompiled_assets)
