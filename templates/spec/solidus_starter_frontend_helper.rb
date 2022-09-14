d1bde49341aca85c4636b3c50e9ff83d55c26381
George Mendoza <gsmendoza@gmail.com>
2022-09-14 15:59:35 +0800

    Do not require 'spree/testing_support/controller_requests'
    The module set the routes of the controller specs to
    Spree::Core::Engine.routes, which are being replaced with
    locally-declared routes.

--- templates/spec/solidus_starter_frontend_helper.rb
+++ templates/spec/solidus_starter_frontend_helper.rb
@@ -15,7 +15,6 @@ require 'ffaker'
 require 'spree/testing_support/authorization_helpers'
 require 'spree/testing_support/url_helpers'
 require 'spree/testing_support/preferences'
-require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/caching'
 require 'spree/testing_support/order_walkthrough'
 require 'spree/testing_support/translations'
@@ -42,7 +41,6 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
   config.include Spree::TestingSupport::UrlHelpers
-  config.include Spree::TestingSupport::ControllerRequests, type: :controller
   config.include Spree::TestingSupport::Preferences
   config.include Spree::TestingSupport::Translations
   config.include Devise::Test::ControllerHelpers, type: :controller
