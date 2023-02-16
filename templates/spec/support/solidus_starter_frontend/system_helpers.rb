8dbf67198bff43abd595fb73026598393ca7d593
Elia Schito <elia@schito.me>
2023-02-16 14:16:38 +0100

    List existing and new payment methods together
    The triple level of radio buttons that was previously used was very
    confusing and broken. Instead of trying to fix it seems better to
    present the customer a single list of options for making the payment.
    
    The list gives precedence to wallet payments as they require less
    clicks.
    
    If no wallet payments are available the first payment method is
    selected.
    
    Whenever a radio is selected elements from the other list are
    deselected, giving the illusion of having a single list.
    
    Each existing and new payment method have clear and distinct areas
    for the input label and for the details. This gives more symmetry
    between existing and new payments partial implementations and a
    cleaner visual layout.

--- templates/spec/support/solidus_starter_frontend/system_helpers.rb
+++ templates/spec/support/solidus_starter_frontend/system_helpers.rb
@@ -8,4 +8,16 @@ module SystemHelpers
 
     click_on 'Continue'
   end
+
+  def find_existing_payment_radio(wallet_source_id)
+    find("[name='order[wallet_payment_source_id]'][value='#{wallet_source_id}']")
+  end
+
+  def find_payment_radio(payment_method_id)
+    find("[name='order[payments_attributes][][payment_method_id]'][value='#{payment_method_id}']")
+  end
+
+  def find_payment_fieldset(payment_method_id)
+    find("fieldset[name='payment-method-#{payment_method_id}']")
+  end
 end
