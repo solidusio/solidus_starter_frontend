dfd9d2a8be39dd55a2c416173b8baa06defb410d
Elia Schito <elia@schito.me>
2023-03-15 19:40:04 +0100

    elia/coverage: [wip]

--- coverage.rb
+++ coverage.rb
@@ -12,21 +12,12 @@ else
   warn "Provide a CODECOV_TOKEN environment variable to enable Codecov uploads"
 end
 
+COVERAGE_ROOT = ENV['COVERAGE_ROOT'] || File.expand_path "#{__dir__}/.."
+
 SimpleCov.start do
-  root ENV['COVERAGE_ROOT'] || File.expand_path('..', SimpleCov.root) # up one level
+  root COVERAGE_ROOT
   enable_for_subprocesses true
   enable_coverage_for_eval
   add_filter %r{sandbox/(db|config|spec|tmp)/}
   track_files "#{SimpleCov.root}/{template.rb,sandbox/**/*.{erb,rb}}"
-
-  at_fork do |pid|
-    SimpleCov.start do
-      command_name "#{command_name} (subprocess: #{pid})"
-
-      # Be quiet, the parent process will be in charge of output and checking coverage totals.
-      print_error_status = false
-      formatter SimpleCov::Formatter::SimpleFormatter
-      minimum_coverage 0
-    end
-  end
 end
