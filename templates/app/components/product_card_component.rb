12b687b7fe0b7cf520b763e75dbb9884477a73cf
Elia Schito <elia@schito.me>
2023-03-20 13:44:27 +0100

    Remove taxon support from the product card

--- templates/app/components/product_card_component.rb
+++ templates/app/components/product_card_component.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 class ProductCardComponent < ViewComponent::Base
-  def initialize(product, taxon: nil, locale: I18n.locale, price: product.master.default_price)
+  def initialize(product, locale: I18n.locale, price: product.master.default_price)
     @product = product
-    @taxon = taxon
     @locale = locale
     @price = price
   end
 
-  attr_reader :product, :taxon, :locale, :price
+  attr_reader :product, :locale, :price
 
   def main_image
     product.gallery.images.first
@@ -16,9 +15,5 @@ class ProductCardComponent < ViewComponent::Base
 
   def display_price
     @display_price ||= price&.money
-  end
-
-  def url
-    @url ||= product_path(product, taxon_id: taxon.try(:id))
   end
 end
--- templates/spec/components/product_card_component_spec.rb
+++ templates/spec/components/product_card_component_spec.rb
@@ -16,23 +16,4 @@ RSpec.describe ProductCardComponent, type: :component do
       expect(rendered_component.css("li .product-card_image a").first[:href]).to eq('/products/123')
     end
   end
-
-  context 'with a taxon' do
-    it "adds a reference to the taxon in the product url" do
-      taxon = build(:taxon, permalink: 'foo-bar', id: 234)
-      product = build(:product, name: "The best product", id: 123, taxons: [taxon])
-      price = build(:price)
-      rendered_component = render_inline(described_class.new(
-        product,
-        price: price,
-        taxon: taxon,
-      ))
-
-      aggregate_failures do
-        expect(rendered_component.css("li .product-card_header a").to_html).to include("The best product")
-        expect(rendered_component.css("li .product-card_header a").first[:href]).to eq('/products/123?taxon_id=234')
-        expect(rendered_component.css("li .product-card_image a").first[:href]).to eq('/products/123?taxon_id=234')
-      end
-    end
-  end
 end
