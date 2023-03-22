3958bd1eb93d76562af328bf56e3d4f4581895b8
Elia Schito <elia@schito.me>
2023-03-22 17:20:17 +0100

    Turn the locale selector into a stimulus controller

--- templates/app/assets/javascripts/locale_selector.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const localeSelector = document.querySelector('.locale-selector select');
-
-  if(localeSelector) {
-    localeSelector.addEventListener('change', (event) => {
-      event.target.form.submit();
-    });
-  }
-});
