0a24c9d1c195f307277ccb9f3c3bd469ed635ed5
Rainer Dema <rainer.dema@gmail.com>
2023-03-22 11:34:50 +0100

    Fix specs to be compliant with recent changes on solidus
    The specs on `solidus_starter_frontend` were failing due
    to recent improvements with taxon/taxonomy factories made
    in the solidus repository, specifically related to:
    https://github.com/solidusio/solidus/pull/4851
    This commit updates the specs to be compatible with the
    latest changes and ensures that the test suite passes
    successfully.

--- templates/spec/helpers/taxons_helper_spec.rb
+++ templates/spec/helpers/taxons_helper_spec.rb
@@ -4,11 +4,11 @@ require 'solidus_starter_frontend_spec_helper'
 
 RSpec.describe TaxonsHelper, type: :helper do
   describe '#taxon_seo_url' do
-    let(:taxon_permalink) { 'ruby-on-rails' }
-    let(:taxon) { create(:taxon, permalink: taxon_permalink) }
+    let(:taxonomy) { create(:taxonomy, name: 'Categories') }
+    let(:taxon) { create(:taxon, name: 'Clothing', taxonomy: taxonomy) }
 
     it 'is the nested taxons path for the taxon' do
-      expect(taxon_seo_url(taxon)).to eq("/t/#{taxon_permalink}")
+      expect(taxon_seo_url(taxon)).to eq("/t/categories/clothing")
     end
   end
 end
