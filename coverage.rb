7e72e1abe065ff9039debab575f793a1ec92315f
Elia Schito <elia@schito.me>
2023-03-22 16:29:29 +0100

    Stop using the deprecated codecov gem
    Ref: https://github.com/solidusio/solidus_stripe/pull/227

--- coverage.rb
+++ coverage.rb
@@ -1,23 +1,42 @@
+# frozen_string_literal: true
+
 require 'coverage'
 require 'simplecov'
 
+SimpleCov.root File.expand_path(__dir__)
+
 if ENV['CODECOV_TOKEN']
-  require 'codecov'
+  require 'simplecov-cobertura'
 
   SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-    SimpleCov::Formatter::Codecov,
+    SimpleCov::Formatter::CoberturaFormatter,
     SimpleCov.formatter,
   ])
 else
   warn "Provide a CODECOV_TOKEN environment variable to enable Codecov uploads"
 end
 
-COVERAGE_ROOT = ENV['COVERAGE_ROOT'] || File.expand_path "#{__dir__}/.."
+def (SimpleCov::ResultAdapter).call(result)
+  result = result.transform_keys do |path|
+    template_path = path.sub(
+      "#{SimpleCov.root}/sandbox/",
+      "#{SimpleCov.root}/templates/"
+    )
+    File.exist?(template_path) ? template_path : path
+  end
+  result.each do |path, coverage|
+    next unless path.end_with?('.erb')
 
+    # Remove the extra trailing lines added by ERB
+    coverage[:lines] = coverage[:lines][...File.read(path).lines.size]
+  end
+  result
+end
+
+warn "Tracking coverage on process #{$$}..."
 SimpleCov.start do
-  root COVERAGE_ROOT
-  enable_for_subprocesses true
+  root __dir__
   enable_coverage_for_eval
   add_filter %r{sandbox/(db|config|spec|tmp)/}
-  track_files "#{SimpleCov.root}/{template.rb,sandbox/**/*.{erb,rb}}"
+  track_files "#{SimpleCov.root}/{sandbox,lib,app}/**/*.{rb,erb}}"
 end
