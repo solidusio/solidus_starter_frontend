d2d68dd4d67700e73da781964b740d8f2a0ffe8b
Elia Schito <elia@schito.me>
2023-05-15 13:04:19 +0200

    Move the ToS check to the checkout stimulus controller

--- templates/app/javascript/controllers/checkout_controller.js
+++ templates/app/javascript/controllers/checkout_controller.js
@@ -1,24 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ['submitButton']
   static values = {
     step: String,
+    submitting: String,
+    termsRequired: String,
   }
-
-  // actions
 
   beforeSubmit(event) {
     if (this.stepValue === 'payment') {
-      this.disableSubmitButtons()
+      this.submitButtonTarget.setAttribute('disabled', true)
     } else if (this.stepValue === 'confirm') {
-      this.disableSubmitButtons()
+      if (this.termsCheckboxTarget.checked) {
+        this.submitButtonTarget.setAttribute('disabled', true)
+        submitButton.innerHTML = this.submittingValue
+      } else {
+        event.preventDefault()
+        alert(this.termsRequiredValue)
+        submitButton.removeAttribute('disabled')
+        submitButton.classList.remove('disabled')
+      }
     }
-  }
-
-  // private
-
-  disableSubmitButtons() {
-    const elements = this.element.querySelectorAll('[type="submit"], [type="image"]');
-    elements.forEach((element) => element.setAttribute("disabled", true))
   }
 }
