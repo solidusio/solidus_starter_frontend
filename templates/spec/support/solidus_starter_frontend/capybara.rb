75fcb6b60e686e8f849631ae09f026430bee12bc
Thomas von Deyen <thomas@vondeyen.com>
2024-08-27 16:37:37 +0200

    Disable search engine popup in system specs
    Since Chrome 128 there is a Search Engine selection
    popup even in headless mode that needs to be accepted.
    This leads to Capybara Element not found exceptions during
    system specs.
    
    Refs: https://github.com/SeleniumHQ/selenium/issues/14438
    (cherry picked from commit c368c169380c13809dfa8d22aee634cecda15717)

--- templates/spec/support/solidus_starter_frontend/capybara.rb
+++ templates/spec/support/solidus_starter_frontend/capybara.rb
@@ -14,7 +14,9 @@ RSpec.configure do |config|
 
   config.before(:each, type: :system, js: true) do |example|
     screen_size = example.metadata[:screen_size] || [1800, 1400]
-    driven_by :selenium, using: :headless_chrome, screen_size: screen_size
+    driven_by(:selenium, using: :headless_chrome, screen_size: screen_size) do |capabilities|
+      capabilities.add_argument("--disable-search-engine-choice-screen")
+    end
   end
 end
 
@@ -22,6 +24,7 @@ Capybara.register_driver :selenium_chrome_headless_docker_friendly do |app|
   browser_options = ::Selenium::WebDriver::Chrome::Options.new
   browser_options.args << '--headless'
   browser_options.args << '--disable-gpu'
+  browser_options.args << '--disable-search-engine-choice-screen'
   # Sandbox cannot be used inside unprivileged Docker container
   browser_options.args << '--no-sandbox'
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
