c27046b9636a89f7e89c7c3c08d193d2a988fea4
Fab <fabian@thobe.eu>
2025-01-29 08:29:48 +0100

    Added missing Boilerplate License
    Integrate correct copyright of 3rd party code used

--- template.rb
+++ template.rb
@@ -24,6 +24,8 @@ with_log['checking versions'] do
 end
 
 # Copied from: https://github.com/mattbrictson/rails-template
+# Copyright (c) 2023 Matt Brictson
+# Licensed under the terms of the MIT License (MIT)
 # Add this template directory to source_paths so that Thor actions like
 # copy_file and template resolve against our source files. If this file was
 # invoked remotely via HTTP, that means the files are not present locally.
